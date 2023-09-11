3229503d510dbce1002cba723ffb980ee186efd2
adarshajit <adarshajit@gmail.com>
2023-09-11 17:35:14 +0530

    feat: incoperate mermaidjs to create diagrams and update the ticket response from JIRA

--- backend/hello.py
+++ backend/hello.py
@@ -20,7 +20,26 @@ jira = JIRA(jira_instance, basic_auth=(username, password))
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    mermaid_code = """
+sequenceDiagram
+    Alice->>John: Hello John, how are you?
+    John-->>Alice: Great!
+    Alice-)John: See you later!
+"""
+    return mermaid_chart(mermaid_code)
+
+def serialize_comment(comment):
+    return {
+        "author": comment.author.displayName,
+        "created": comment.created,
+        "body": comment.body
+    }
+
+def serialize_attachment(attachment):
+    return {
+        "filename": attachment.filename,
+        "url": attachment.content,
+    }
 
 
 @app.route("/tickets", methods = ["GET"])
@@ -29,11 +48,24 @@ def get_all_tickets():
     issues = jira.search_issues(jql_query)
 
     tickets = []
+
     for issue in issues:
+        hasAttachments = len(issue.fields.attachment) > 0
+        hasComments = len(issue.fields.comment.comments) > 0
+        attachments = [serialize_attachment(attachment) for attachment in issue.fields.attachment]
+        comments = [serialize_comment(comment) for comment in issue.fields.comment.comments]
+        
         ticket = {
             "key": issue.key,
             "summary": issue.fields.summary,
-            "description": issue.fields.description
+            "description": issue.fields.description,
+            "issueType": issue.fields.issuetype.name,
+            "labels": issue.fields.labels,
+            "attachments": attachments,
+            "hasAttachments": hasAttachments,
+            "comments": comments,
+            "hasComments": hasComments,
+            "created": issue.fields.created,
         }
 
         tickets.append(ticket)
@@ -86,3 +118,12 @@ def extract_text_from_pdf(pdf_content):
         pdf_text += page.extract_text()
     
     return pdf_text
+
+def mermaid_chart(mindmap_code):
+    html_code = f"""
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <div class="mermaid">{mindmap_code}</div>
+    <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+    <script>mermaid.initialize({{startOnLoad:true}});</script>
+    """
+    return html_code
