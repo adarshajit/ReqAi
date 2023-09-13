6debb7db01c8ddc6ec5f8d839ef21820199e7a14
adarshajit <adarshajit@gmail.com>
2023-09-13 13:15:38 +0530

    chore: add requirements.txt file and script to run both client and server

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Navigate to the backend folder
+cd backend
+
+pip install -r requirements.txt
+
+# Start the Flask API server in the background
+flask --app hello run
+
+# Navigate to the client folder
+cd ../client
+
+# Start the React development server
+npm run dev
