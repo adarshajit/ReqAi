5b11e41cd4d640866382bc64c8d3ec7745deed9a
adarshajit <adarshajit@gmail.com>
2023-09-13 13:46:37 +0530

    chore: update package version

--- startup.sh
+++ startup.sh
@@ -6,10 +6,12 @@ cd backend
 pip install -r requirements.txt
 
 # Start the Flask API server in the background
-flask --app hello run
+flask --app hello run --debug &
 
 # Navigate to the client folder
 cd ../client
+
+npm i
 
 # Start the React development server
 npm run dev
