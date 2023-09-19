de9dcc4b9728c22e1d120011359a46beb445756f
LAPTOP-CNN29T1R\gaurav <gaurav.akumar07@gmail.com>
2023-09-19 08:03:10 +0530

    'changes_for_bard_implemented'

--- backend/palmApiIntegration.py
+++ backend/palmApiIntegration.py
@@ -6,7 +6,9 @@ import google.ai.generativelanguage as glm
 import google.generativeai as palm
 import os
 import json
+import re
 
+from utils.extract_text_from_pdf import extract_text_from_pdf
 from google.generativeai.client import get_default_text_client
 from google.generativeai.types import text_types
 from google.generativeai.types import model_types
@@ -26,33 +28,36 @@ def createJira():
   """Hit the PaLM API and return the generated text."""
   key = os.environ.get("PALM_API_KEY")
   url_formation = os.environ.get("URL")
-  if request.is_json:
-        data = request.get_json()
-  if "requirements" in data:
-            prompt = data["requirements"]
-  prompt_dict = {"prompt": {"text": prompt}}
-  if "fontStyle" in data:
-            model_name = data["fontStyle"]
-  prompt_dict = {"prompt": {"text": prompt}}
-  headers = {
+  pdf_file = request.files['pdf_file']
+  pdf_content = pdf_file.read()
+  pdf_text = extract_text_from_pdf(pdf_content)
+  prompt_dict = {"prompt": {"text":"""Break down the following Business Requirements Document into user stories in JSON format which JIRA
+rest APIs expect for story creation for given jira project key. Create separate jsons for each user story.
+Each json should include the following sections: 1. Summary: this should be a short heading for the
+story(this summary should be in plain english and SHOULD NOT be same as the user story or in user
+story format) 2. Description: This should include three sections, namely user story, Acceptance Criterea
+and Test Cases 3. Impact Analysis: This should include an impact analysis for the user story 4. KEY: This
+should be XXXX
+Business Requirement Document:"""+ pdf_text}}
+  headers =  {
   "Content-Type": "application/json",
   "Accept": "application/json",
   }
- 
- 
-  
-  
-  url = url_formation+"{}:generateText?key=".format(model_name)
+  url = url_formation+":generateText?key="
   url += key
-  
   response = requests.post(url, headers=headers, data=json.dumps(prompt_dict))
 
   if response.status_code == 200:
     json_output = response.json()["candidates"][0]["output"]
+    code = re.sub(r'^```\n', '', json_output)
+    code = re.sub(r'\n```$', '', json_output)
+    code = re.sub(r'```json','', json_output)
+    code = re.sub(r'`','',json_output)
     
-    return json_output
+    return code
   else:
     return None
+  
 
 
 if __name__ == '__main__':
