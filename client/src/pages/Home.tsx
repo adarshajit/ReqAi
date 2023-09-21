768ebb7d85ebfedb9fdd78bd37ff95014a5694f0
adarshajit <adarshajit@gmail.com>
2023-09-21 17:17:00 +0530

    fix: update classNames to keep images as center

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="flex gap-10">
           {FEATURES.map((feature) => {
             return (
-              <div className="flex flex-col gap-6 w-64 justify-center features-center">
+              <div className="flex flex-col gap-6 w-64 justify-center items-center">
                 <img src={feature.image.file} width={feature.image.size} />
                 <p>{feature.description}</p>
                 <Link to={feature.url} className="w-full">
