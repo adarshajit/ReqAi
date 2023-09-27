d20db67c9696c7d48f302b942d3bf3da519c3ee8
adarshajit <adarshajit@gmail.com>
2023-09-27 19:38:22 +0530

    fix: create mermaidjs code based on diagramType and ticket description

--- backend/routes/bard.py
+++ backend/routes/bard.py
@@ -3,7 +3,7 @@ import requests
 import os
 import json
 import re
-from .utils.extract_text_from_pdf import extract_text_from_pdf
+from ..utils.extract_text_from_pdf import extract_text_from_pdf
 from jira import JIRA
 
 palmApi = Blueprint('palmApi', __name__)
@@ -11,20 +11,18 @@ palmApi = Blueprint('palmApi', __name__)
 key = os.environ.get("PALM_API_KEY")
 url_formation = os.environ.get("URL")
 
+username = os.environ.get("EMAIL_ID")
+password = os.environ.get("API_TOKEN")
+jira_instance = os.environ.get("JIRA_INSTANCE")
+
+jira = JIRA(jira_instance, basic_auth=(username, password))
+
 @palmApi.route('/api/createJira', methods=['POST'])
 def createJira():
   pdf_file = request.files['pdf_file']
   pdf_content = pdf_file.read()
   pdf_text = extract_text_from_pdf(pdf_content)
-  prompt_dict = {"prompt": {"text":"""Break down the following Business Requirements Document into One single story in an array format which JIRA
-rest APIs expect for story creation for given jira project key. format is as follows: {
-                            summary: <A short heading for the ticket>,
-                            description: <an elaborate explanation of this ticket>,
-                            acceptanceCriteria: <acceptance criteria that needs to be satisfied>,
-                            testCases: <test scenarios that needs to be tested>,
-                            impactAnalysis: <an impact analysis for the feature>
-} the output response should not start with 'json'
-Business Requirement Document:"""+ pdf_text}}
+  prompt_dict = {"prompt": {"text":"""Break down the following Business Requirements Document into user stories in JSON format which JIRA rest APIs expect for story creation for given jira project key. Create separate jsons for each user story. Each json should include the following sections: 1. Summary: this should be a short heading for the story(this summary should be in plain english and SHOULD NOT be same as the user story or in user story format) 2. Description: This should include four sections, namely user story, Acceptance Criterea and Test Cases, Impact Analysis  4. KEY: This should be XXXX. Format should be like this {"summary": "", "description":{"User Story":"","Acceptance Criteria":["","",""],"Test Cases":["","",""], "Impact Analysis":{"positive":"","negative":""}},"key":""}. Output should be a string. Business Requirement Document:"""+ pdf_text}}
   headers =  {
   "Content-Type": "application/json",
   "Accept": "application/json",
@@ -36,14 +34,14 @@ Business Requirement Document:"""+ pdf_text}}
   if response.status_code == 200:
     json_output = response.json()["candidates"][0]["output"]
     code = re.sub(r'^```\n', '', json_output)
-    code = re.sub(r'\n```$', '', json_output)
-    code = re.sub(r'```json','', json_output)
-    code = re.sub(r'`','',json_output)
-    
-    return code
+    code = re.sub(r'\n```$', '', code)
+    code = re.sub(r'```json', '', code)
+    code = re.sub(r'^\s*json\s*{', '{', code)
+    code = re.sub(r'`', '', code)
+
+    return json.dumps(code)
   else:
     return None
-
 
 @palmApi.route('/api/createBrd', methods=['POST'])
 def createBrd():
@@ -60,18 +58,21 @@ def createBrd():
   response = requests.post(url, headers=headers, data=json.dumps(prompt_dict))
   return response.json()["candidates"][0]["output"]
 
-@palmApi.route('/api/createDiagram/<ticket_id>', methods=['POST'])
+@palmApi.route('/api/createDiagram', methods=['POST'])
 def createDiagram():
-  """Hit the PaLM API and return the diagram."""
+  body = request.get_json()
+  ticket_id = body.get("ticketId")
+  diagramType = body.get("diagramType")
+
   issue = jira.issue(str(ticket_id))
-  prompt_dict = {"prompt": {"text":"""For the following user story, please output mermaid.js code to create a sequence diagram. The code should follow the standard format expected by mermaid and should be as detailed as possible:"""+ issue.fields.description}}
+
+  prompt_dict = {"prompt": {"text":"For the following user story, please output mermaid.js code to create a" + diagramType + "diagram. The code should follow the standard format expected by mermaid and should be as detailed as possible:"+ issue.fields.description}}
 
   headers =  {
   "Content-Type": "application/json",
   "Accept": "application/json",
   }
   
-
   url_formation = os.environ.get("URL")
   url = url_formation+":generateText?key="
   url += key
@@ -88,7 +89,3 @@ def createDiagram():
   else:
     return None
   
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
