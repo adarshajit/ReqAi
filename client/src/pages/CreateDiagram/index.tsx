1efcdb8e3ebb684bd7d6ec201642ccc9810742e2
adarshajit <adarshajit@gmail.com>
2023-09-29 12:42:19 +0530

    feat: add success page

--- client/src/pages/CreateDiagram/index.tsx
+++ client/src/pages/CreateDiagram/index.tsx
@@ -5,10 +5,15 @@ import { INITIAL_FORM_DATA } from '../../constants';
 import Tickets from './Tickets';
 import DiagramType from './DiagramType';
 import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
+import SuccessImage from '../../assets/success.svg';
+import Spinner from '../../components/Spinner';
 
 const CreateDiagram: FC = () => {
   const [formData, setFormData] = useState<DiagramFormData>(INITIAL_FORM_DATA);
+  const [showSuccessPage, setShowSuccessPage] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const { currentStepIndex, step, next, back } = useMultiStepForm([
     <Tickets {...formData} updateFields={updateFields} />,
     <DiagramType {...formData} updateFields={updateFields} />,
@@ -22,40 +27,61 @@ const CreateDiagram: FC = () => {
 
   const handleSubmit = async () => {
     try {
+      setLoading(true);
       await axios.post(`http://localhost:5000/api/createDiagram`, formData);
-      toast.success('Diagram generated successfully');
+      setShowSuccessPage(true);
     } catch (err) {
-      toast.error('Uh Oh! There seem to be some issue.');
+      console.error(err);
+    } finally {
+      setLoading(false);
     }
   };
 
-  return (
-    <div className="flex flex-col gap-4 mt-24">
-      <div className="flex justify-between">
-        <button className="btn btn-neutral" onClick={back} disabled={currentStepIndex === 0}>
-          Back
-        </button>
-        <ul className="steps">
-          <li className={`step ${currentStepIndex === 0 ? 'step-neutral' : ''}`}>Choose Ticket</li>
-          <li className={`step ${currentStepIndex === 1 ? 'step-neutral' : ''}`}>
-            Choose Diagram Style
-          </li>
-        </ul>
-        <button
-          className="btn btn-neutral"
-          onClick={currentStepIndex === 1 ? handleSubmit : next}
-          disabled={
-            (currentStepIndex === 0 && formData.ticketId === '') ||
-            (currentStepIndex === 1 && formData.diagramType === '')
-          }
-        >
-          {currentStepIndex === 1 ? 'Submit' : 'Next'}
-        </button>
+  if (loading) return <Spinner />;
+
+  if (showSuccessPage)
+    return (
+      <>
+        <div className="hero min-h-screen">
+          <div className="hero-content text-center flex flex-col gap-16">
+            <img src={SuccessImage} width={300} />
+            <Link className="w-full btn btn-neutral" to="/">
+              Return to home
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+
+  if (!showSuccessPage)
+    return (
+      <div className="flex flex-col gap-4 mt-24">
+        <div className="flex justify-between">
+          <button className="btn btn-neutral" onClick={back} disabled={currentStepIndex === 0}>
+            Back
+          </button>
+          <ul className="steps">
+            <li className={`step ${currentStepIndex === 0 ? 'step-neutral' : ''}`}>
+              Choose Ticket
+            </li>
+            <li className={`step ${currentStepIndex === 1 ? 'step-neutral' : ''}`}>
+              Choose Diagram Style
+            </li>
+          </ul>
+          <button
+            className="btn btn-neutral"
+            onClick={currentStepIndex === 1 ? handleSubmit : next}
+            disabled={
+              (currentStepIndex === 0 && formData.ticketId === '') ||
+              (currentStepIndex === 1 && formData.diagramType === '')
+            }
+          >
+            {currentStepIndex === 1 ? 'Submit' : 'Next'}
+          </button>
+        </div>
+        {step}
       </div>
-      {step}
-      <ToastContainer />
-    </div>
-  );
+    );
 };
 
 export default CreateDiagram;
