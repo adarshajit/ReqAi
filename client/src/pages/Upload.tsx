adab5ce3135a81bfeca20e9d59c6ff57dfab835d
adarshajit <adarshajit@gmail.com>
2023-09-30 17:40:33 +0530

    feat: view generated ticket information on btn click

--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -4,11 +4,14 @@ import Spinner from '../components/Spinner';
 import UploadImage from '../assets/upload.svg';
 import { convertStringToJSON, issueTypeLabel } from '../utils';
 import { ToastContainer, toast } from 'react-toastify';
+import { Ticket } from '../types';
+import GeneratedTicketDetails from './GeneratedTicketDetails';
 
 const Upload: FC = () => {
   const [file, setFile] = useState<File | string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const [userStories, setUserStories] = useState<any>(null);
+  const [ticket, setTicket] = useState<Ticket | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
     e.preventDefault();
@@ -43,6 +46,15 @@ const Upload: FC = () => {
     }
   };
 
+  const handleViewClick = (ticketSummary: string | null): void => {
+    if (ticketSummary) {
+      const selectedTicket = userStories.find(
+        (userStory: any) => userStory.summary === ticketSummary
+      );
+      setTicket(selectedTicket);
+    }
+  };
+
   if (loading)
     return <Spinner message="Crafting user stories with the finesse of a master chef ✍️" />;
 
@@ -72,25 +84,33 @@ const Upload: FC = () => {
 
   if (userStories)
     return (
-      <div className="flex flex-col gap-6 max-w-2xl mt-24">
+      <div className="flex flex-col gap-6 max-w-3xl my-24">
         <h1 className="text-3xl font-bold leading-10 my-12">
-          Pick the user stories that you want to be created in JIRA
+          Select the user stories you want to have in JIRA ✨
         </h1>
-        {userStories.map((ticket: any, id: string) => (
-          <div
-            key={id}
-            className="card border-2 border-grey-200 transition duration-300 ease-in-out"
-          >
-            <div className="flex flex-col p-8 gap-4">
-              <div className="flex justify-between">
-                {issueTypeLabel('Story')} <input type="checkbox" className="checkbox" />
+        <div className="drawer drawer-end flex flex-col gap-4 max-w-2xl">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          {userStories.map((ticket: any, id: string) => (
+            <div key={id} className="card border-2 border-grey-200">
+              <div className="flex flex-col p-8 gap-4">
+                <div className="flex justify-between">
+                  {issueTypeLabel('Story')} <input type="checkbox" className="checkbox" />
+                </div>
+                <h2 className="card-title">{ticket.summary}</h2>
+                <p>{ticket.description.substring(0, 200)}...</p>
+                <hr />
+                <label
+                  htmlFor="my-drawer"
+                  className="btn btn-neutral drawer-button"
+                  onClick={() => handleViewClick(ticket.summary)}
+                >
+                  View
+                </label>
               </div>
-              <h2 className="card-title">{ticket.summary}</h2>
-              <p>{ticket.description.substring(0, 200)}...</p>
             </div>
-          </div>
-        ))}
-
+          ))}
+          <GeneratedTicketDetails ticket={ticket} />
+        </div>
         <ToastContainer />
       </div>
     );
